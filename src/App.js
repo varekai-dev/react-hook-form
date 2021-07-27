8ff6fa4cb6b8989b5e7b0d323e5654f40798eeb6
varekai-dev <serhijsav@gmail.com>
2021-07-27 06:46:31 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Step1 = () => <>Step 1</>;
+const Step2 = () => <>Step 1</>;
+const Step3 = () => <>Step 1</>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Step1} />
+          <Route exact path="/step2" component={Step2} />
+          <Route exact path="/step3" component={Step3} />
+          <Route exact path="/result" component={Result} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export const Header = () => {
+  return <Typography>The Ultimate Form Challenge</Typography>;
+};
